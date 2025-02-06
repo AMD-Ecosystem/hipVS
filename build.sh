b6deae83dff21315abbf219e66b8aec68c21746c
Ajit Mistry <55892788+ajit283@users.noreply.github.com>
2025-02-06 21:57:05 +0100

    Go API - [WIP] (#212)
    This PR adds a Go API. It's far from completion and still work in progress. Feel free to suggest improvements!
    
    Authors:
      - Ajit Mistry (https://github.com/ajit283)
      - Corey J. Nolet (https://github.com/cjnolet)
      - Ben Frederickson (https://github.com/benfred)
    
    Approvers:
      - Ben Frederickson (https://github.com/benfred)
      - Gil Forsyth (https://github.com/gforsyth)
    
    URL: https://github.com/rapidsai/cuvs/pull/212

--- build.sh
+++ build.sh
@@ -35,7 +35,7 @@ ARGS=$*
 # scripts, and that this script resides in the repo dir!
 REPODIR=$(cd $(dirname $0); pwd)
 
-VALIDARGS="clean libcuvs python rust java docs tests bench-ann examples --uninstall  -v -g -n --compile-cuda --compile-static-lib --allgpuarch --no-mg --no-cpu --cpu-only --no-shared-libs --no-nvtx --show_depr_warn --incl-cache-stats --time -h"
+VALIDARGS="clean libcuvs python rust go java docs tests bench-ann examples --uninstall  -v -g -n --compile-cuda --compile-static-lib --allgpuarch --no-mg --no-cpu --cpu-only --no-shared-libs --no-nvtx --show_depr_warn --incl-cache-stats --time -h"
 HELP="$0 [<target> ...] [<flag> ...] [--cmake-args=\"<args>\"] [--cache-tool=<tool>] [--limit-tests=<targets>] [--limit-bench-ann=<targets>] [--build-metrics=<filename>]
  where <target> is:
    clean            - remove all existing build artifacts and configuration (start over)
@@ -43,6 +43,7 @@ HELP="$0 [<target> ...] [<flag> ...] [--cmake-args=\"<args>\"] [--cache-tool=<to
                       around the C++ code.
    python           - build the cuvs/hipvs Python package
    rust             - build the cuvs/hipvs Rust bindings
+   go               - build the cuvs/hipvs Go bindings
    java             - build the cuvs/hipvs Java bindings
    docs             - build the documentation
    tests            - build the tests
@@ -477,6 +478,13 @@ if (( ${NUMARGS} == 0 )) || hasArg rust; then
     cd ${REPODIR}/rust
     cargo build --examples --lib
     cargo test
+fi
+
+# Build the cuvs Go bindings
+if (( ${NUMARGS} == 0 )) || hasArg go; then
+    cd ${REPODIR}/go
+    go build ./...
+    go test ./...
 fi
 
 # Build the cuvs Java bindings
