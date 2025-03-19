b490d4d48f1d4dcaafafcabb3ec45a61f50196ed
gpuCI <38199262+GPUtester@users.noreply.github.com>
2025-03-19 13:41:56 -0400

    Merge pull request #776 from rapidsai/branch-25.04
    Forward-merge branch-25.04 into branch-25.06

--- build.sh
+++ build.sh
@@ -54,6 +54,7 @@ HELP="$0 [<target> ...] [<flag> ...] [--cmake-args=\"<args>\"] [--cache-tool=<to
    -g                          - build for debug
    -n                          - no install step
    --uninstall                 - uninstall files for specified targets which were built and installed prior
+   --cpu-only                  - build CPU only components without CUDA. Currently only applies to bench-ann.
    --limit-tests               - semicolon-separated list of test executables to compile (e.g. NEIGHBORS_TEST;CLUSTER_TEST)
    --limit-bench-ann           - semicolon-separated list of ann benchmark executables to compute (e.g. HNSWLIB_ANN_BENCH;RAFT_IVF_PQ_ANN_BENCH)
    --allgpuarch                - build for all supported GPU architectures
@@ -411,8 +412,14 @@ if (( ${NUMARGS} == 0 )) || hasArg python; then
     # Build and install cuvs pip package
     SKBUILD_CMAKE_ARGS="-DCMAKE_CXX_COMPILER=hipcc;-DCMAKE_PREFIX_PATH=${INSTALL_PREFIX};${EXTRA_CMAKE_ARGS}" \
         SKBUILD_BUILD_OPTIONS="-j${PARALLEL_LEVEL}" \
-        python -m pip install --no-build-isolation ${REPODIR}/python/cuvs
+        python -m pip install --no-build-isolation  --no-deps --config-settings rapidsai.disable-cuda=true ${REPODIR}/python/cuvs
 fi
+
+# Build and (optionally) install the cuvs-bench Python package
+if (( NUMARGS == 0 )) || (hasArg bench-ann && ! hasArg -n); then
+    python -m pip install --no-build-isolation --no-deps --config-settings rapidsai.disable-cuda=true ${REPODIR}/python/cuvs_bench
+fi
+
 
 # Build the cuvs Rust bindings
 if (( ${NUMARGS} == 0 )) || hasArg rust; then
