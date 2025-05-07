8aaeecddf87fd13bb719a374698fc1397d69d28d
Jinsol Park <jinsolp@nvidia.com>
2025-05-07 13:12:28 -0700

    [FIX] Revert negated InnerProduct distance for NN Descent (#859)
    Closing https://github.com/rapidsai/cuvs/issues/849
    
    Many parts of NN Descent is written on top of the assumption that smaller distance is closer.
    Therefore, using the current trick (using -(dot product) values) to make it consistent with other metrics to be sorted in ascending order, then negating the value at the end should be the neatest solution.
    
    Authors:
      - Jinsol Park (https://github.com/jinsolp)
    
    Approvers:
      - Ben Frederickson (https://github.com/benfred)
    
    URL: https://github.com/rapidsai/cuvs/pull/859

--- cpp/tests/neighbors/ann_nn_descent.cuh
+++ cpp/tests/neighbors/ann_nn_descent.cuh
@@ -170,13 +170,6 @@ class AnnNNDescentTest : public ::testing::TestWithParam<AnnNNDescentInputs> {
         raft::resource::sync_stream(handle_);
       }
 
-      if (ps.metric == cuvs::distance::DistanceType::InnerProduct) {
-        std::transform(
-          distances_naive.begin(), distances_naive.end(), distances_naive.begin(), [](auto x) {
-            return -x;
-          });
-      }
-
       double min_recall =
         ps.metric ==
             cuvs::distance::DistanceType::InnerProduct  // TODO: (HIP/AMD) Investigate the decreased
