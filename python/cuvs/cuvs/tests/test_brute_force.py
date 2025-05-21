aeee6d6542836de84fbf92a0c9e08081141ca3fc
sukchoud <sukriti.choudhary@amd.com>
2025-05-21 21:54:34 +0000

    (port) Python enablement for hipvs build.sh: added python package building option for hipvs and build option for C tests/lib all_rocm_arch-x86_64.yaml: adds new file that lists dependencies for conda AMD build python/CMakeLists.txt: update Cmake for hip ivf_pq.pxd: Update file to include hip dependencies nn_descent and binary CMakeLists.txt: remove logger test_brute_force.py: pull upstream change test_ivf_pq.py: relax recall

--- python/cuvs/cuvs/tests/test_brute_force.py
+++ python/cuvs/cuvs/tests/test_brute_force.py
@@ -12,13 +12,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# MIT License
+#
+# Modifications Copyright (c) 2025 Advanced Micro Devices, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
 
 import numpy as np
 import pytest
+from cuvs.neighbors import brute_force, filters
 from pylibraft.common import device_ndarray
 from scipy.spatial.distance import cdist
-
-from cuvs.neighbors import brute_force, filters
 
 
 @pytest.mark.parametrize("n_index_rows", [32, 100])
@@ -134,7 +155,9 @@ def test_prefiltered_brute_force_knn(
     index = np.random.random_sample((n_index_rows, n_cols)).astype(dtype)
     queries = np.random.random_sample((n_query_rows, n_cols)).astype(dtype)
     bitmap = create_sparse_array(
-        (np.ceil(n_query_rows * n_index_rows / 32).astype(np.uint32)), sparsity
+        # TODO(AMD/HIP): upstream change for bug fix
+        int(np.ceil(n_query_rows * n_index_rows / 32)),
+        sparsity,
     )
 
     is_min = metric != "inner_product"
