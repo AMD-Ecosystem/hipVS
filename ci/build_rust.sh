6f56c2a28f4f9b731bb6aa49a321dc30a13b51d1
Ben Frederickson <ben@benfrederickson.com>
2025-06-06 12:09:43 -0700

    Merge pull request #990 from benfred/rust_publishing_fix2
    Update rust/cuvs-sys

--- ci/build_rust.sh
+++ ci/build_rust.sh
@@ -32,3 +32,8 @@ export LIBCLANG_PATH=$(dirname $(find /opt/conda -name libclang.so | head -n 1))
 echo "LIBCLANG_PATH=$LIBCLANG_PATH"
 
 bash ./build.sh rust
+
+# Also test out that we can publish cuvs-sys via a dry-run
+pushd ./rust/cuvs-sys
+cargo publish --dry-run
+popd
