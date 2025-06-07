1c7c57a56ac5686903f5b3591991ddc03aa372e2
MithunR <mithunr@nvidia.com>
2025-06-06 18:51:17 -0700

    Add C-API method to fetch cuVS version from Java. (#935)
    Fixes #930.
    
    This commit adds a C API function to fetch the cuVS version.
    
    Authors:
      - MithunR (https://github.com/mythrocks)
      - Corey J. Nolet (https://github.com/cjnolet)
    
    Approvers:
      - Chris Hegarty (https://github.com/ChrisHegarty)
      - Corey J. Nolet (https://github.com/cjnolet)
      - James Lamb (https://github.com/jameslamb)
    
    URL: https://github.com/rapidsai/cuvs/pull/935

--- cpp/include/cuvs/core/c_api.h
+++ cpp/include/cuvs/core/c_api.h
@@ -41,6 +41,10 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+static const uint16_t CUVS_VERSION_MAJOR = 25;
+static const uint16_t CUVS_VERSION_MINOR = 06;
+static const uint16_t CUVS_VERSION_PATCH = 00;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -50,7 +54,7 @@ extern "C" {
  * @{
  */
 /**
- * @brief An enum denoting return values for function calls
+ * @brief An enum denoting error statuses for function calls
  *
  */
 typedef enum { CUVS_ERROR, CUVS_SUCCESS } cuvsError_t;
@@ -192,6 +196,15 @@ cuvsError_t cuvsRMMHostAlloc(void** ptr, size_t bytes);
  * @return cuvsError_t
  */
 cuvsError_t cuvsRMMHostFree(void* ptr, size_t bytes);
+
+/**
+ * @brief Get the version of the cuVS library
+ * @param[out] major Major version
+ * @param[out] minor Minor version
+ * @param[out] patch Patch version
+ * @return cuvsError_t
+ */
+cuvsError_t cuvsVersionGet(uint16_t* major, uint16_t* minor, uint16_t* patch);
 
 /** @} */
 
