bd3e68485c4ad3c974cb42a2521177e2473a6d33
Kevin Joseph <kevin.joseph@amd.com>
2025-06-18 20:56:29 +0000

    [CAGRA] Relax recall for graph_build_algo::ITERATIVE_CAGRA_SEARCH

--- cpp/tests/neighbors/ann_cagra.cuh
+++ cpp/tests/neighbors/ann_cagra.cuh
@@ -1504,14 +1504,23 @@ static std::vector<AnnCagraInputs> relax_recall(std::vector<AnnCagraInputs> inpu
   // We gently relax the recall targets:
   //  - For MULTI_CTA/AUTO modes, cap at 95% of original recall
   //  - For IVF_PQ builds, cap at 90% of original recall
+  //  - For ITERATIVE_CAGRA_SEARCH cap at 97% of original recall
   // See issue: https://github.com/AMD-AI/hipVS/issues/22
   for (auto& input : inputs) {
     double reduction_factor = 1.0;
     if (input.algo == search_algo::MULTI_CTA || input.algo == search_algo::AUTO) {
       reduction_factor = std::min(reduction_factor, 0.95);
     }
-    if (input.build_algo == graph_build_algo::IVF_PQ) {
-      reduction_factor = std::min(reduction_factor, 0.90);
+    switch (input.build_algo) {
+      case graph_build_algo::IVF_PQ: {
+        reduction_factor = std::min(reduction_factor, 0.90);
+        break;
+      }
+      case graph_build_algo::ITERATIVE_CAGRA_SEARCH: {
+        reduction_factor = std::min(reduction_factor, 0.97);
+        break;
+      }
+      default: break;
     }
     input.min_recall *= reduction_factor;
   }
@@ -1520,7 +1529,7 @@ static std::vector<AnnCagraInputs> relax_recall(std::vector<AnnCagraInputs> inpu
 }
 
 const std::vector<AnnCagraInputs> inputs           = relax_recall(generate_inputs());
-const std::vector<AnnCagraInputs> inputs_addnode   = generate_addnode_inputs();
+const std::vector<AnnCagraInputs> inputs_addnode   = relax_recall(generate_addnode_inputs());
 const std::vector<AnnCagraInputs> inputs_filtering = generate_filtering_inputs();
 
 }  // namespace cuvs::neighbors::cagra
