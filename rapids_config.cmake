6716384dbf76ae5b0c7a80dd48dae10b07a8c685
Charrier, Dominic Etienne <DominicEtienne.Charrier@amd.com>
2025-11-11 09:31:03 +0100

    rapids_config.cmake: set *SCRIPT_BRANCH to release/rocmds-25.10

--- rapids_config.cmake
+++ rapids_config.cmake
@@ -63,6 +63,10 @@ if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/CUVS_RAPIDS-${RAPIDS_VERSION_MAJOR_MI
   else()
     set(RAPIDS_CMAKE_SCRIPT_BRANCH release/rocmds-25.10)
   endif()
+  if(NOT DEFINED ENV{RAPIDS_CMAKE_VERSION})
+    message(STATUS "RAPIDS_CMAKE_REPO is not set. Using '2.0.0'.")
+    set(ENV{RAPIDS_CMAKE_VERSION} 2.0.0)
+  endif()
   set(URL
       "https://raw.githubusercontent.com/ROCm-DS/ROCmDS-cmake/${RAPIDS_CMAKE_SCRIPT_BRANCH}/RAPIDS.cmake"
   )
