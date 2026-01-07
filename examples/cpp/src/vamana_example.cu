rapidsai/cuvs

Add error checking for our C example code (#1171)
We weren't consistently checking the return value of cuda and cuvs functions in our C examples.

This caused problems when we tried to adapt the SQ code to an example, and it was returning invalid results. The root cause was because a function was failing, but we didn't notice because the pattern has been to ignore the error codes.

Enforce best practices and check the result of every cuvs and cuda function call in our c examples.

Also:
* enable -Wall and -Werror to the c examples for extra compile time checks
* add examples to CODEOWNERS
*  ensure that clang-format runs on the example files

Authors:
  - Ben Frederickson (https://github.com/benfred)

Approvers:
  - Corey J. Nolet (https://github.com/cjnolet)
  - Gil Forsyth (https://github.com/gforsyth)

URL: https://github.com/rapidsai/cuvs/pull/1171

examples/cpp/src/vamana_example.cu
@@ -28,11 +28,15 @@
 #include "common.cuh"
 
 template <typename T>
-void vamana_build_and_write(raft::device_resources const &dev_resources,
+void vamana_build_and_write(raft::device_resources const& dev_resources,
                             raft::device_matrix_view<const T, int64_t> dataset,
-                            std::string out_fname, int degree, int visited_size,
-                            float max_fraction, int iters,
-                            std::string codebook_prefix) {
+                            std::string out_fname,
+                            int degree,
+                            int visited_size,
+                            float max_fraction,
+                            int iters,
+                            std::string codebook_prefix)
+{
   using namespace cuvs::neighbors;
 
   // use default index parameters
@@ -41,19 +45,18 @@ void vamana_build_and_write(raft::device_resources const &dev_resources,
   index_params.visited_size = visited_size;
   index_params.graph_degree = degree;
   index_params.vamana_iters = iters;
-  index_params.codebooks = vamana::deserialize_codebooks(codebook_prefix, dataset.extent(1));
+  index_params.codebooks    = vamana::deserialize_codebooks(codebook_prefix, dataset.extent(1));
 
   std::cout << "Building Vamana index (search graph)" << std::endl;
 
   auto start = std::chrono::system_clock::now();
   auto index = vamana::build(dev_resources, index_params, dataset);
-  auto end = std::chrono::system_clock::now();
+  auto end   = std::chrono::system_clock::now();
   std::chrono::duration<double> elapsed_seconds = end - start;
 
   std::cout << "Vamana index has " << index.size() << " vectors" << std::endl;
-  std::cout << "Vamana graph has degree " << index.graph_degree()
-            << ", graph size [" << index.graph().extent(0) << ", "
-            << index.graph().extent(1) << "]" << std::endl;
+  std::cout << "Vamana graph has degree " << index.graph_degree() << ", graph size ["
+            << index.graph().extent(0) << ", " << index.graph().extent(1) << "]" << std::endl;
 
   std::cout << "Time to build index: " << elapsed_seconds.count() << "s\n";
 
@@ -64,26 +67,32 @@ void vamana_build_and_write(raft::device_resources const &dev_resources,
   serialize(dev_resources, out_fname + ".sector_aligned", index, false, true);
 }
 
-void usage() {
-  printf("Usage: ./vamana_example <data filename> <output filename> <graph "
-         "degree> <visited_size> <max_fraction> <iterations> <(optional) "
-         "codebook prefix>\n");
+void usage()
+{
+  printf(
+    "Usage: ./vamana_example <data filename> <output filename> <graph "
+    "degree> <visited_size> <max_fraction> <iterations> <(optional) "
+    "codebook prefix>\n");
   printf("Input file expected to be binary file of fp32 vectors.\n");
   printf("Graph degree sizes supported: 32, 64, 128, 256\n");
   printf("Visited_size must be > degree and a power of 2.\n");
   printf("max_fraction > 0 and <= 1. Typical values are 0.06 or 0.1.\n");
   printf("Default iterations = 1, increase for better quality graph.\n");
-  printf("Optional path prefix to pq pivots and rotation matrix files. Expects pq pivots file at ${codebook_prefix}_pq_pivots.bin and rotation matrix file at ${codebook_prefix}_pq_pivots.bin_rotation_matrix.bin.\n");
+  printf(
+    "Optional path prefix to pq pivots and rotation matrix files. Expects pq pivots file at "
+    "${codebook_prefix}_pq_pivots.bin and rotation matrix file at "
+    "${codebook_prefix}_pq_pivots.bin_rotation_matrix.bin.\n");
   exit(1);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[])
+{
   raft::device_resources dev_resources;
 
   // Set pool memory resource with 1 GiB initial pool size. All allocations use
   // the same pool.
   rmm::mr::pool_memory_resource<rmm::mr::device_memory_resource> pool_mr(
-      rmm::mr::get_current_device_resource(), 1024 * 1024 * 1024ull);
+    rmm::mr::get_current_device_resource(), 1024 * 1024 * 1024ull);
   rmm::mr::set_current_device_resource(&pool_mr);
 
   // Alternatively, one could define a pool allocator for temporary arrays (used
@@ -93,25 +102,28 @@ int main(int argc, char *argv[]) {
   // limit. raft::resource::set_workspace_to_pool_resource(dev_resources, 2 *
   // 1024 * 1024 * 1024ull);
 
-  if (argc != 7 && argc != 8)
-    usage();
+  if (argc != 7 && argc != 8) usage();
 
-  std::string data_fname = (std::string)(argv[1]); // Input filename
-  std::string out_fname = (std::string)argv[2];    // Output index filename
-  int degree = atoi(argv[3]);
-  int max_visited = atoi(argv[4]);
-  float max_fraction = atof(argv[5]);
-  int iters = atoi(argv[6]);
+  std::string data_fname      = (std::string)(argv[1]);  // Input filename
+  std::string out_fname       = (std::string)argv[2];    // Output index filename
+  int degree                  = atoi(argv[3]);
+  int max_visited             = atoi(argv[4]);
+  float max_fraction          = atof(argv[5]);
+  int iters                   = atoi(argv[6]);
   std::string codebook_prefix = "";
   if (argc >= 8)
-    codebook_prefix = (std::string)argv[7]; // Path prefix to pq pivots and rotation matrix files
+    codebook_prefix = (std::string)argv[7];  // Path prefix to pq pivots and rotation matrix files
 
   // Read in binary dataset file
-  auto dataset =
-      read_bin_dataset<uint8_t, int64_t>(dev_resources, data_fname, INT_MAX);
+  auto dataset = read_bin_dataset<uint8_t, int64_t>(dev_resources, data_fname, INT_MAX);
 
   // Simple build example to create graph and write to a file
-  vamana_build_and_write<uint8_t>(
-      dev_resources, raft::make_const_mdspan(dataset.view()), out_fname, degree,
-      max_visited, max_fraction, iters, codebook_prefix);
+  vamana_build_and_write<uint8_t>(dev_resources,
+                                  raft::make_const_mdspan(dataset.view()),
+                                  out_fname,
+                                  degree,
+                                  max_visited,
+                                  max_fraction,
+                                  iters,
+                                  codebook_prefix);
 }

AMD-Ecosystem/hipVS

examples/cpp/src/vamana_example.cu
@@ -45,11 +45,15 @@
 #include "common.cuh"
 
 template <typename T>
-void vamana_build_and_write(raft::device_resources const &dev_resources,
+void vamana_build_and_write(raft::device_resources const& dev_resources,
                             raft::device_matrix_view<const T, int64_t> dataset,
-                            std::string out_fname, int degree, int visited_size,
-                            float max_fraction, int iters,
-                            std::string codebook_prefix) {
+                            std::string out_fname,
+                            int degree,
+                            int visited_size,
+                            float max_fraction,
+                            int iters,
+                            std::string codebook_prefix)
+{
   using namespace cuvs::neighbors;
 
   // use default index parameters
@@ -58,19 +62,18 @@ void vamana_build_and_write(raft::device_resources const &dev_resources,
   index_params.visited_size = visited_size;
   index_params.graph_degree = degree;
   index_params.vamana_iters = iters;
-  index_params.codebooks = vamana::deserialize_codebooks(codebook_prefix, dataset.extent(1));
+  index_params.codebooks    = vamana::deserialize_codebooks(codebook_prefix, dataset.extent(1));
 
   std::cout << "Building Vamana index (search graph)" << std::endl;
 
   auto start = std::chrono::system_clock::now();
   auto index = vamana::build(dev_resources, index_params, dataset);
-  auto end = std::chrono::system_clock::now();
+  auto end   = std::chrono::system_clock::now();
   std::chrono::duration<double> elapsed_seconds = end - start;
 
   std::cout << "Vamana index has " << index.size() << " vectors" << std::endl;
-  std::cout << "Vamana graph has degree " << index.graph_degree()
-            << ", graph size [" << index.graph().extent(0) << ", "
-            << index.graph().extent(1) << "]" << std::endl;
+  std::cout << "Vamana graph has degree " << index.graph_degree() << ", graph size ["
+            << index.graph().extent(0) << ", " << index.graph().extent(1) << "]" << std::endl;
 
   std::cout << "Time to build index: " << elapsed_seconds.count() << "s\n";
 
@@ -81,26 +84,32 @@ void vamana_build_and_write(raft::device_resources const &dev_resources,
   serialize(dev_resources, out_fname + ".sector_aligned", index, false, true);
 }
 
-void usage() {
-  printf("Usage: ./vamana_example <data filename> <output filename> <graph "
-         "degree> <visited_size> <max_fraction> <iterations> <(optional) "
-         "codebook prefix>\n");
+void usage()
+{
+  printf(
+    "Usage: ./vamana_example <data filename> <output filename> <graph "
+    "degree> <visited_size> <max_fraction> <iterations> <(optional) "
+    "codebook prefix>\n");
   printf("Input file expected to be binary file of fp32 vectors.\n");
   printf("Graph degree sizes supported: 32, 64, 128, 256 (must be greater than or equal to the device warp/wavefront size)\n");
   printf("Visited_size must be > degree and a power of 2.\n");
   printf("max_fraction > 0 and <= 1. Typical values are 0.06 or 0.1.\n");
   printf("Default iterations = 1, increase for better quality graph.\n");
-  printf("Optional path prefix to pq pivots and rotation matrix files. Expects pq pivots file at ${codebook_prefix}_pq_pivots.bin and rotation matrix file at ${codebook_prefix}_pq_pivots.bin_rotation_matrix.bin.\n");
+  printf(
+    "Optional path prefix to pq pivots and rotation matrix files. Expects pq pivots file at "
+    "${codebook_prefix}_pq_pivots.bin and rotation matrix file at "
+    "${codebook_prefix}_pq_pivots.bin_rotation_matrix.bin.\n");
   exit(1);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[])
+{
   raft::device_resources dev_resources;
 
   // Set pool memory resource with 1 GiB initial pool size. All allocations use
   // the same pool.
   rmm::mr::pool_memory_resource<rmm::mr::device_memory_resource> pool_mr(
-      rmm::mr::get_current_device_resource(), 1024 * 1024 * 1024ull);
+    rmm::mr::get_current_device_resource(), 1024 * 1024 * 1024ull);
   rmm::mr::set_current_device_resource(&pool_mr);
 
   // Alternatively, one could define a pool allocator for temporary arrays (used
@@ -110,25 +119,28 @@ int main(int argc, char *argv[]) {
   // limit. raft::resource::set_workspace_to_pool_resource(dev_resources, 2 *
   // 1024 * 1024 * 1024ull);
 
-  if (argc != 7 && argc != 8)
-    usage();
+  if (argc != 7 && argc != 8) usage();
 
-  std::string data_fname = (std::string)(argv[1]); // Input filename
-  std::string out_fname = (std::string)argv[2];    // Output index filename
-  int degree = atoi(argv[3]);
-  int max_visited = atoi(argv[4]);
-  float max_fraction = atof(argv[5]);
-  int iters = atoi(argv[6]);
+  std::string data_fname      = (std::string)(argv[1]);  // Input filename
+  std::string out_fname       = (std::string)argv[2];    // Output index filename
+  int degree                  = atoi(argv[3]);
+  int max_visited             = atoi(argv[4]);
+  float max_fraction          = atof(argv[5]);
+  int iters                   = atoi(argv[6]);
   std::string codebook_prefix = "";
   if (argc >= 8)
-    codebook_prefix = (std::string)argv[7]; // Path prefix to pq pivots and rotation matrix files
+    codebook_prefix = (std::string)argv[7];  // Path prefix to pq pivots and rotation matrix files
 
   // Read in binary dataset file
-  auto dataset =
-      read_bin_dataset<float, int64_t>(dev_resources, data_fname, INT_MAX);
+  auto dataset = read_bin_dataset<float, int64_t>(dev_resources, data_fname, INT_MAX);
 
   // Simple build example to create graph and write to a file
-  vamana_build_and_write<float>(
-      dev_resources, raft::make_const_mdspan(dataset.view()), out_fname, degree,
-      max_visited, max_fraction, iters, codebook_prefix);
+  vamana_build_and_write<float>(dev_resources,
+                                  raft::make_const_mdspan(dataset.view()),
+                                  out_fname,
+                                  degree,
+                                  max_visited,
+                                  max_fraction,
+                                  iters,
+                                  codebook_prefix);
 }